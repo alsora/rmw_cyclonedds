1e09e160e9df293c20ec137080bdfbe31a559c3f
alberto <alberto.soragna@gmail.com>
2019-07-11 16:46:49 +0100

    find cyclone dds not required

--- cyclonedds_cmake_module/cmake/Modules/FindCycloneDDS.cmake
+++ cyclonedds_cmake_module/cmake/Modules/FindCycloneDDS.cmake
@@ -33,7 +33,7 @@
 
 set(CycloneDDS_FOUND FALSE)
 
-find_package(CycloneDDS REQUIRED CONFIG)
+find_package(CycloneDDS CONFIG QUIET)
 
 #find_library(CycloneDDS_LIBRARY_RELEASE
 #  NAMES cyclonedds-${cyclonedds_MAJOR_MINOR_VERSION} cyclonedds)
@@ -42,29 +42,31 @@ find_library(CycloneDDS_LIBRARY_RELEASE
 #find_library(CycloneDDS_LIBRARY_DEBUG
 #  NAMES cycloneddsd-${cyclonedds_MAJOR_MINOR_VERSION})
 
-set(CycloneDDS_INCLUDE_DIR get_target_property(VAR CycloneDDS::ddsc INTERFACE_INCLUDE_DIRECTORIES))
+if (CycloneDDS_FOUND)
+  set(CycloneDDS_INCLUDE_DIR get_target_property(VAR CycloneDDS::ddsc INTERFACE_INCLUDE_DIRECTORIES))
 
-if(CycloneDDS_LIBRARY_RELEASE AND CycloneDDS_LIBRARY_DEBUG)
-  set(CycloneDDS_LIBRARIES
-    optimized ${CycloneDDS_LIBRARY_RELEASE}
-    debug ${CycloneDDS_LIBRARY_DEBUG}
+  if(CycloneDDS_LIBRARY_RELEASE AND CycloneDDS_LIBRARY_DEBUG)
+    set(CycloneDDS_LIBRARIES
+      optimized ${CycloneDDS_LIBRARY_RELEASE}
+      debug ${CycloneDDS_LIBRARY_DEBUG}
+    )
+  elseif(CycloneDDS_LIBRARY_RELEASE)
+    set(CycloneDDS_LIBRARIES
+      ${CycloneDDS_LIBRARY_RELEASE}
+    )
+  elseif(CycloneDDS_LIBRARY_DEBUG)
+    set(CycloneDDS_LIBRARIES
+      ${CycloneDDS_LIBRARY_DEBUG}
+    )
+  else()
+    set(CycloneDDS_LIBRARIES "")
+  endif()
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(CycloneDDS
+    FOUND_VAR CycloneDDS_FOUND
+    REQUIRED_VARS
+      CycloneDDS_INCLUDE_DIR
+      CycloneDDS_LIBRARIES
   )
-elseif(CycloneDDS_LIBRARY_RELEASE)
-  set(CycloneDDS_LIBRARIES
-    ${CycloneDDS_LIBRARY_RELEASE}
-  )
-elseif(CycloneDDS_LIBRARY_DEBUG)
-  set(CycloneDDS_LIBRARIES
-    ${CycloneDDS_LIBRARY_DEBUG}
-  )
-else()
-  set(CycloneDDS_LIBRARIES "")
 endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CycloneDDS
-  FOUND_VAR CycloneDDS_FOUND
-  REQUIRED_VARS
-    CycloneDDS_INCLUDE_DIR
-    CycloneDDS_LIBRARIES
-)
